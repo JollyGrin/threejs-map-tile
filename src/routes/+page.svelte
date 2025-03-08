390f1b8857da0a5920630df750553f7c58749ab7
JollyGrin <lol@dean.lol>
2025-03-08 16:40:14 +0100

    add git

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
 	import Scene from './Scene.svelte';
 </script>
 
+<div class=" fixed top-3 left-3 z-50 opacity-50">
+	<a href="https://github.com/JollyGrin/threejs-map-tile ">git </a>
+</div>
+
 <div class="h-screen w-screen">
 	<Canvas>
 		<Scene />
